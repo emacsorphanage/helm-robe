7348d0bc0251b51979554ea678b970fd01c0efe9
Syohei YOSHIDA <syohex@gmail.com>
2015-12-09 12:53:33 +0900

    Bump up version and update Changes

--- helm-robe.el
+++ helm-robe.el
@@ -4,7 +4,7 @@
 
 ;; Author: Syohei YOSHIDA <syohex@gmail.com>
 ;; URL:https://github.com/syohex/emacs-helm-robe
-;; Version: 0.01
+;; Version: 0.02
 ;; Package-Requires: ((helm "1.7.7"))
 
 ;; This program is free software; you can redistribute it and/or modify
